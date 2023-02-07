ceb377aeb96d9867ec0764805c099f4f2052bc8f
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-02-07 21:16:24 +0000

    First commit deletes notes from last.

--- TestingSandboxTests/TestingSandboxTests.swift
+++ TestingSandboxTests/TestingSandboxTests.swift
@@ -3,18 +3,14 @@
 //  TestingSandboxTests
 //
 //  Created by Yoli on 07/02/2023.
-//gives us access to things we wouldn't normally have access to. 
 @testable import TestingSandbox
 import XCTest
 
 final class TestingSandboxTests: XCTestCase {
-//sut = system under test.
     func test_failingThrows() throws {
         let sut = DataModel()
-        //clearer to catch any errors and print since when in 6 months time we make some changes it's a lot clearer as to what's happening.
         do {
             try sut.goingToFail()
-            //I think judging from the doc these are considered assertions. What we do when something goes wrong.
         } catch {
             XCTFail("The goingToFail method should not throw an error")
         }
