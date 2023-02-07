66a64174dbff42ae55fbe4426b0b03d1808b62b3
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-02-07 16:28:24 +0000

    Initial Commit

---
+++ TestingSandbox/TestingSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingSandboxApp.swift
+//  TestingSandbox
+//
+//  Created by Yoli on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestingSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
